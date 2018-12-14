112ec5c669f15b23728971775942d94fdc65caef
Alina Goaga <goaga.alina@gmail.com>
2018-12-14 11:17:01 +0000

    Added form to input name and birthday

--- birthday.rb
+++ birthday.rb
@@ -4,29 +4,13 @@ class Birthday < Sinatra::Base
   get '/' do
     erb :index
   end
-  #
-  # post '/names' do
-  #   player_1 = Player.new(params[:player_1_name])
-  #   player_2 = Player.new(params[:player_2_name])
-  #   $game = Game.new(player_1, player_2)
-  #   redirect '/play'
-  # end
-  #
-  # get '/play' do
-  #   @game = $game
-  #   erb :play
-  # end
-  #
-  # get '/attack' do
-  #   @game = $game
-  #   @game.attack(@game.opponent_of(@game.current_turn))
-  #   erb :attack
-  # end
-  #
-  # post '/switch' do
-  #   $game.switch
-  #   redirect('/play')
-  # end
+
+  post '/details' do
+    @name = params[:name]
+    @day = params[:day]
+    @month = params[:month]
+    redirect '/'
+  end
 
   run! if app_file == $PROGRAM_NAME
 end
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -3,4 +3,12 @@ feature 'Welcome page' do
     visit('/')
     expect(page).to have_content 'Hello there!'
   end
+
+  scenario 'the user inputs their name and birthday (date and month)' do
+    visit('/')
+    fill_in :name, with: 'Alina'
+    fill_in :day, with: '25'
+    fill_in :month, with: 'June'
+    click_button 'Go!'
+  end
 end
